79ba1b7042d0d197da1eee5c2b735d9357f88795
Said Rodrigues <coderflemis@gmail.com>
2024-05-04 07:35:11 -0300

    add mappings to java

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -4,13 +4,21 @@ vim.opt_local.softtabstop = 4
 vim.opt_local.ts = 4
 vim.opt_local.expandtab = true
 
-local status, jdtls = pcall(require, "jdtls")
+local nmap = function(keys, func, desc)
+  if desc then
+    desc = 'LSP: ' .. desc
+  end
+
+  vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+end
+
+local status, jdtls = pcall(require, 'jdtls')
 if not status then
   return
 end
 
 local function capabilities()
-  local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+  local status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
   if status_ok then
     return cmp_nvim_lsp.default_capabilities()
   end
@@ -19,9 +27,9 @@ local function capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities.textDocument.completion.completionItem.resolveSupport = {
     properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
+      'documentation',
+      'detail',
+      'additionalTextEdits',
     },
   }
 
@@ -29,61 +37,57 @@ local function capabilities()
 end
 
 local function directory_exists(path)
-  local f = io.popen("cd " .. path)
-  local ff = f:read "*all"
+  local f = io.popen('cd ' .. path)
+  local ff = f:read '*all'
 
-  if ff:find "ItemNotFoundException" then
+  if ff:find 'ItemNotFoundException' then
     return false
   else
     return true
   end
 end
 
-local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-local root_dir = require("jdtls.setup").find_root(root_markers)
+local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
+local root_dir = require('jdtls.setup').find_root(root_markers)
 
 -- calculate workspace dir
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. project_name
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = vim.fn.stdpath 'data' .. '/site/java/workspace-root/' .. project_name
 if directory_exists(workspace_dir) then
 else
-  os.execute("mkdir " .. workspace_dir)
+  os.execute('mkdir ' .. workspace_dir)
 end
 -- get the mason install path
-local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
+local install_path = require('mason-registry').get_package('jdtls'):get_install_path()
 
 local bundles = {}
-local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
-vim.list_extend(bundles, vim.split(vim.fn.glob(mason_path .. "packages/java-test/extension/server/*.jar"), "\n"))
-vim.list_extend(
-  bundles,
+local mason_path = vim.fn.glob(vim.fn.stdpath 'data' .. '/mason/')
+vim.list_extend(bundles, vim.split(vim.fn.glob(mason_path .. 'packages/java-test/extension/server/*.jar'), '\n'))
+vim.list_extend(bundles,
   vim.split(
-    vim.fn.glob(mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"),
-    "\n"
-  )
-)
+  vim.fn.glob(mason_path .. 'packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar'), '\n'))
 
 local config = {
   cmd = {
-    "java",
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-javaagent:" .. install_path .. "/lombok.jar",
-    "-Xms2g",
-    "-Xmx3g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    "-jar",
-    vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
-    "-configuration",
-    install_path .. "/config_linux",
-    "-data",
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-javaagent:' .. install_path .. '/lombok.jar',
+    '-Xms2g',
+    '-Xmx3g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+    '-jar',
+    vim.fn.glob(install_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
+    '-configuration',
+    install_path .. '/config_linux',
+    '-data',
     workspace_dir,
   },
   capabilities = capabilities(),
@@ -105,20 +109,20 @@ local config = {
     signatureHelp = { enabled = true },
     completion = {
       favoriteStaticMembers = {
-        "org.hamcrest.MatcherAssert.assertThat",
-        "org.hamcrest.Matchers.*",
-        "org.hamcrest.CoreMatchers.*",
-        "org.junit.jupiter.api.Assertions.*",
-        "java.util.Objects.requireNonNull",
-        "java.util.Objects.requireNonNullElse",
-        "org.mockito.Mockito.*",
+        'org.hamcrest.MatcherAssert.assertThat',
+        'org.hamcrest.Matchers.*',
+        'org.hamcrest.CoreMatchers.*',
+        'org.junit.jupiter.api.Assertions.*',
+        'java.util.Objects.requireNonNull',
+        'java.util.Objects.requireNonNullElse',
+        'org.mockito.Mockito.*',
       },
       filteredTypes = {
-        "com.sun.*",
-        "io.micrometer.shaded.*",
-        "java.awt.*",
-        "jdk.*",
-        "sun.*",
+        'com.sun.*',
+        'io.micrometer.shaded.*',
+        'java.awt.*',
+        'jdk.*',
+        'sun.*',
       },
     },
     sources = {
@@ -127,34 +131,58 @@ local config = {
         staticStarThreshold = 9999,
       },
     },
-    contentProvider = { preferred = "fernflower" },
+    contentProvider = { preferred = 'fernflower' },
     format = {
       enabled = true,
       settings = {
-        url = vim.fn.stdpath "config" .. "/ftplugin/intellij-java-code-style.xml",
+        url = vim.fn.stdpath 'config' .. '/ftplugin/intellij-java-code-style.xml',
       },
     },
   },
 
   init_options = {
     bundles = {
-      vim.fn.glob(
-        mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
-        "\n"
-      ),
+      vim.fn.glob(mason_path .. 'packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar',
+        '\n'),
     },
   },
 }
 
-config["on_attach"] = function(client, bufnr)
+config['on_attach'] = function(client, bufnr)
   local _, _ = pcall(vim.lsp.codelens.refresh)
-  require("jdtls.dap").setup_dap_main_class_configs()
-  jdtls.setup_dap { hotcodereplace = "auto" }
-  require("user.lsp.handlers").on_attach(client, bufnr)
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  -- pee `:help K` for why this keymap
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- pesser used LSP functionality
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, '[W]orkspace [L]ist Folders')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
+
+  require('jdtls.dap').setup_dap_main_class_configs()
+  jdtls.setup_dap { hotcodereplace = 'auto' }
+  require('user.lsp.handlers').on_attach(client, bufnr)
 end
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.java" },
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  pattern = { '*.java' },
   callback = function()
     local _, _ = pcall(vim.lsp.codelens.refresh)
   end,
