bcd9ca5e1a8e4129c5936ecd6f085a9f38ae7e05
Said Rodrigues <coderflemis@gmail.com>
2025-01-21 08:41:37 -0300

    feat: update provider to use claude

--- init.lua
+++ init.lua
@@ -705,7 +705,7 @@ require('lazy').setup({
     lazy = false,
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
-      provider = "copilot",
+      provider = "claude",
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
