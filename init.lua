8e21940bbdb994f6e2e8cd8b72811d8bc13525fc
Said Rodrigues <coderflemis@gmail.com>
2025-01-30 07:57:44 -0300

    add helm

--- init.lua
+++ init.lua
@@ -678,7 +678,15 @@ require('lazy').setup({
   {
     'mbbill/undotree'
   },
-  { 'towolf/vim-helm', ft = 'helm' },
+  {
+    "qvalentin/helm-ls.nvim",
+    ft = "helm",
+    opts = {
+      conceal_templates = {
+        enabled = true,
+      },
+    }
+  },
   {
     'chomosuke/typst-preview.nvim',
     ft = 'typst',
@@ -715,9 +723,9 @@ require('lazy').setup({
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "hrsh7th/nvim-cmp",          -- autocompletion for avante commands and mentions
+      "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",    -- for providers='copilot'
+      "zbirenbaum/copilot.lua",      -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -6,6 +6,14 @@
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
+local js_based_languages = {
+  "typescript",
+  "javascript",
+  "typescriptreact",
+  "javascriptreact",
+  "vue",
+}
+
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
@@ -25,13 +33,25 @@ return {
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
+    { 'leoluz/nvim-dap-go', ft = "go" },
     {
       'mfussenegger/nvim-dap-python',
+      ft = "python",
       config = function()
         require("dap-python").setup(vim.g.python_host_prog)
         require("dap-python").test_runner = "pytest"
       end,
+    },
+    {
+      "microsoft/vscode-js-debug",
+      -- After install, build it and rename the dist directory to out
+      build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+      version = "1.*",
+      ft = js_based_languages,
+    },
+    {
+      "Joakker/lua-json5",
+      build = "./install.sh",
     },
   },
   config = function()
@@ -85,6 +105,77 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    local mason_registry = require("mason-registry")
+    local js_debug_path = mason_registry.get_package("js-debug-adapter"):get_install_path()
+
+    for _, language in ipairs(js_based_languages) do
+      dap.configurations[language] = {
+        -- Debug single nodejs files
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+        },
+        -- Debug nodejs processes (make sure to add --inspect when you run the process)
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach",
+          processId = require("dap.utils").pick_process,
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+        },
+        -- Debug web applications (client side)
+        {
+          type = "pwa-chrome",
+          request = "launch",
+          name = "Launch & Debug Chrome",
+          url = function()
+            local co = coroutine.running()
+            return coroutine.create(function()
+              vim.ui.input({
+                prompt = "Enter URL: ",
+                default = "http://localhost:3000",
+              }, function(url)
+                if url == nil or url == "" then
+                  return
+                else
+                  coroutine.resume(co, url)
+                end
+              end)
+            end)
+          end,
+          webRoot = vim.fn.getcwd(),
+          protocol = "inspector",
+          sourceMaps = true,
+          userDataDir = false,
+        },
+      }
+    end
+
+    for _, adapter in ipairs({
+      "pwa-node",
+      "pwa-chrome",
+      "pwa-msedge",
+      "pwa-extensionHost",
+    }) do
+      dap.adapters[adapter] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            js_debug_path .. "/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
+        },
+      }
+    end
 
     -- Install golang specific config
     require('dap-go').setup()
